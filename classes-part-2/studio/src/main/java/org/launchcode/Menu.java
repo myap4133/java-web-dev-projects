aadcdb21db6964a783b0afd654bf802d7e6e22c9
may2fn <may2fn@umsystem.edu>
2023-09-07 20:13:42 -0500

    Completed studio 4

--- classes-part-2/studio/src/main/java/org/launchcode/Menu.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Menu.java
@@ -1,32 +1,91 @@
 package org.launchcode;
 
+
 import java.util.ArrayList;
 import java.util.Date;
+
 
 public class Menu {
     private Date lastUpdated;
     private ArrayList<MenuItem> items;
+
 
     public Menu(Date d, ArrayList<MenuItem> i) {
         this.lastUpdated = d;
         this.items = i;
     }
 
+
     public void setLastUpdated(Date lastUpdated) {
         this.lastUpdated = lastUpdated;
     }
+
 
     public void setItems(ArrayList<MenuItem> items) {
         this.items = items;
     }
 
+
     public Date getLastUpdated() {
         return lastUpdated;
     }
 
+
     public ArrayList<MenuItem> getItems() {
         return items;
     }
+
+
+    public void addNewItem(MenuItem item){
+        if(!items.contains(item)){
+            item.setNew(true);
+            items.add(item);
+            setLastUpdated(new Date());
+            displayMenu();
+        }
+        else{
+            System.out.println("Menu already contains item. Please try again.\n");
+        }
+    }
+
+
+    public void removeItem(MenuItem item){
+        if(items.contains(item)){
+            items.remove(item);
+            setLastUpdated(new Date());
+            displayMenu();
+        }
+        else{
+            System.out.println("Menu does not contain this item. Please try again.\n");
+        }
+    }
+
+
+    public void displayMenu(){
+        System.out.println("Today's Menu");
+        for(MenuItem item: items){
+            if(item.isNew()){
+                System.out.println("New Item");
+            }
+            System.out.println(item.getCategory());
+            System.out.println(item.getDescription() + "                " + item.getPrice());
+        }
+        System.out.println();
+    }
+
+
+    public void displayItem(MenuItem item){
+        if(items.contains(item)){
+            int ind = items.indexOf(item);
+            if(items.get(ind).isNew()){
+                System.out.println("New Item");
+            }
+            System.out.println(items.get(ind).getCategory());
+            System.out.println(items.get(ind).getDescription() + "                " + items.get(ind).getPrice());
+            System.out.println();
+        }
+        else{
+            System.out.println("Menu does not contain this item. Please try again.\n");
+        }
+    }
 }
-
-
--- classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
@@ -1,10 +1,15 @@
 package org.launchcode;
 
+
+import java.util.Objects;
+
+
 public class MenuItem {
     private double price;
     private String description;
     private String category;
     private boolean isNew;
+
 
     public MenuItem(double p, String d, String c, boolean iN) {
         this.price = p;
@@ -13,20 +18,60 @@ public class MenuItem {
         this.isNew = iN;
     }
 
+
     public void setPrice(double price) {
         this.price = price;
     }
+
 
     public void setDescription(String description) {
         this.description = description;
     }
 
+
     public void setCategory(String category) {
         this.category = category;
     }
 
+
     public void setNew(boolean aNew) {
         isNew = aNew;
     }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public String getCategory() {
+        return category;
+    }
+
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuItem menuItem = (MenuItem) o;
+        return Double.compare(getPrice(), menuItem.getPrice()) == 0 && isNew() == menuItem.isNew() && Objects.equals(getDescription(), menuItem.getDescription()) && Objects.equals(getCategory(), menuItem.getCategory());
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPrice(), getDescription(), getCategory(), isNew());
+    }
 }
+
 
